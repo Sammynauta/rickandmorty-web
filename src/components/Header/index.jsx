b98b6ebc66d4460fa7b4597cd38e6c8f5e7e8977
Sammynauta <samuka.o.diogo@gmail.com>
2021-09-07 21:46:52 -0300

    correcao em Header e ainda falta criar footer.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import logo from "../../assets/logorickandmorty.png";
 
-export default function Header () {
-    return (
-        <div>
-            <img src={logo} alt="logo" width='500px' height='300px'/>
-        </div>
-    )
+export default function Header() {
+  return (
+    <div className="header">
+      <div>
+        <img src={logo} alt="logo" width="500px" height="300px" />
+      </div>
+
+    </div>
+  );
 }
